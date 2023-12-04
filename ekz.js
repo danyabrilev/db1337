180fb7bb4122ba27f5b5524ad051484400d54f44
danyabrilev <dbrylev18@gmail.com>
2023-12-04 12:48:22 +0300

    aa

--- ekz.js
+++ ekz.js
@@ -1,1 +1,11 @@
-hello world
+const getMaxNumber = (a, b) => {
+    let maxNumber;
+    if (a > b) {
+        maxNumber = a;
+    } else {
+        maxNumber = b;
+    }
+    return maxNumber;
+};
+console.log(getMaxNumber(150, 15)); 
+
